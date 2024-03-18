1deaf363f3470fa4000bbf0c7b405d024924114a
Marcelo Botega Fontana <marcelobotega@gmail.com>
2024-03-18 16:25:19 +0100

    feat: first commit

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
 console.time('No route rule')
 const response = await useFetch('http://localhost:3001/delayed-response', {
   timeout: 1000,
-  retry: false,
 });
 console.log(response.error.value)
 console.timeEnd('No route rule')
@@ -10,7 +9,6 @@ console.timeEnd('No route rule')
 console.time('With route rule')
 const response2 = await useFetch('/delayed-response', {
   timeout: 1000,
-  retry: false,
 });
 console.log(response2.error.value)
 console.timeEnd('With route rule')
